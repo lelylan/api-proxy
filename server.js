c7cc2117f6762a66fca08ac3607e17c7ab801203
Andrea Reginato <andrea.reginato@gmail.com>
2012-08-16 22:48:47 +0200

    Added setting system

--- server.js
+++ server.js
@@ -1,24 +1,19 @@
 var nock = require('nock');
 var http = require('http');
 var httpProxy = require('http-proxy');
-
-var config = {
-  'devices':   'localhost:3000/devices',
-  'types':     'localhost:3001/types',
-  'locations': 'localhost:3002/locations'
-}
+var settings = require('konphyg')(__dirname + '/settings')('settings');
 
 var options = {
   router: {
-    'localhost/devices':      config.devices,
-    'localhost/consumptions': config.devices,
-    'localhost/histories':    config.devices,
-    'localhost/types':        config.types,
-    'localhost/properties':   config.types,
-    'localhost/functions':    config.types,
-    'localhost/statuses':     config.types,
-    'localhost/categories':   config.types,
-    'localhost/locations':    config.locations,
+    'localhost/devices':      settings.devices,
+    'localhost/consumptions': settings.devices,
+    'localhost/histories':    settings.devices,
+    'localhost/types':        settings.types,
+    'localhost/properties':   settings.types,
+    'localhost/functions':    settings.types,
+    'localhost/statuses':     settings.types,
+    'localhost/categories':   settings.types,
+    'localhost/locations':    settings.locations,
   }
 }
 
