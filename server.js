2e620861364106ae45aaff49119c55e4ce4ce9f7
Andrea Reginato <andrea.reginato@gmail.com>
2012-08-16 17:57:46 +0200

    Basicly working test

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ var nock = require('nock');
 var http = require('http');
 var httpProxy = require('http-proxy');
 
+var config = {
+  'devices':   'localhost:3000/devices',
+  'types':     'localhost:3000/types',
+  'locations': 'localhost:3000/locations'
+}
+
 var options = {
   router: {
-    'localhost/devices':   'localhost:3000/devices',
-    'localhost/types':     'localhost:3001/types',
-    'localhost/locations': 'localhost:3002/locations',
+    'localhost/devices':   config.devices,
+    'localhost/types':     config.types,
+    'localhost/locations': config.locations,
   }
 }
 
--- spec/server.spec.js
+++ spec/server.spec.js
@@ -1,18 +1,36 @@
 var nock    = require('nock');
+var http    = require('http');
 var request = require('request');
 
 describe('when requires a resource', function() {
 
-  var host    = 'http://localhost:3000';
-  var path    = '/devices'
   var fixture = __dirname + '/fixtures/devices.json';
-  var scope   = nock(host).post(path).replyWithFile(201, fixture);
+  var scope = nock('http://localhost:8000').post('devices').replyWithFile(200, fixture);
 
-  it('keeps status code', function(done) { 
-    request('http://localhost:8000' + path, function(error, res, body) {
-      console.log(error)
-      expect(res.statusCode).toEqual('201');
-      done();
+  //it('keeps status code', function(done) { 
+  //request('http://localhost:8000/devices', function(error, res, body) {
+  //console.log(error);
+  ////expect(res.statusCode).toEqual('201');
+  //done();
+  //});
+  //});
+
+  it("get gets mocked", function(done) {
+    var dataCalled = false
+
+    var scope = nock('http://www.google.com')
+    .get('/')
+    .reply(200, "Hello World!");
+
+    var req = http.request({
+      host: "www.google.com"
+      , path: '/'
+      , port: 80
+    }, function(res) {
+      expect(res.statusCode).toEqual('200');
     });
+
+    req.end();
+    done();
   });
 });
