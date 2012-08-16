3c53e4620a4a6a91944985f1d378b9527e36c135
Andrea Reginato <andrea.reginato@gmail.com>
2012-08-16 16:05:11 +0200

    Basic working request forward

--- server.js
+++ server.js
@@ -1,46 +1,14 @@
-// ----------------
-// Initialization
-// ----------------
 var nock = require('nock');
 var http = require('http');
 var httpProxy = require('http-proxy');
 
-// --------------
-// Proxy server
-// --------------
-
-var options = { router: { 'http://localhost:9000/devices': 'devices.lelylan.com' } }
-
-var proxyServer = httpProxy.createServer(function(req, res, proxy) {
-  proxy.proxyRequest(req, res, options);
-}).listen(8000);
-
-
-// -------------
-// Node Server
-// -------------
-
-var httpServer = http.createServer(function (req, res) {
-  //res.writeHead(200, { 'Content-Type': 'text/plain' });
-  //res.write('request successfully proxied: ' + req.url +'\n' + JSON.stringify(req.headers, true, 2));
-  //res.end();
-
-  parseLocationHeader(res);
-  res.end();
-}).listen(9000);
-
-
-// ----------------
-// Helper Methods
-// ----------------
-
-function parseLocationHeader(res) {
-  console.log(res.statusCode)
-  location = res.getHeader('Location');
-  console.log(location);
-  if (location != null) {
-    location.replace('devices.lelylan.com', 'api.lelylan.com');
-    res.setHeader('Location', location);
-    console.log(res.getHeader('Location'))
+var options = {
+  host: 'localhost', port: 3000,
+  router: { 
+    'localhost:8000/devices': 'http://localhost:3000'
   }
 }
+
+httpProxy.createServer(function(req, res, proxy) {
+  proxy.proxyRequest(req, res, options);
+}).listen(8000);
--- spec/server.spec.js
+++ spec/server.spec.js
@@ -3,24 +3,16 @@ var request = require('request');
 
 describe('when requires a resource', function() {
 
-  var host    = 'https://devices.lelylan.com';
+  var host    = 'http://localhost:3000';
   var path    = '/devices'
-  var headers = { 'Location': 'https://devices.lelylan.com/devices/03aef92001293dfae982738' };
   var fixture = __dirname + '/fixtures/devices.json';
-  var scope   = nock(host).post(path).replyWithFile(201, fixture, headers);
+  var scope   = nock(host).post(path).replyWithFile(201, fixture);
 
   it('keeps status code', function(done) { 
-    request('http://localhost:9000' + path, function(error, res, body) {
+    request('http://localhost:8000' + path, function(error, res, body) {
+      console.log(error)
       expect(res.statusCode).toEqual('201');
       done();
     });
   });
-
-  //it('updates location header', function(done) {
-    //request('http://localhost:9000' + path, function(error, response, body) {
-      //console.log('YOOOOOO');
-      //expect(response.getHeader('Location')).toEqual("api.lelylan.com/devices");
-      //done();
-    //});
-  //});
 });
