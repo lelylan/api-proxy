959dfd49a9d6fd003cff4c22a9e6669d240342b4
Andrea Reginato <andrea.reginato@gmail.com>
2012-08-23 17:33:05 +0200

    Set localhost as env variable

--- web.js
+++ web.js
@@ -5,22 +5,21 @@ var httpProxy = require('http-proxy');
 settings = {
   "devices":   process.env.DEVICES_URI,
   "types":     process.env.TYPES_URI,
-  "locations": process.env.LOCATIONS_URI
+  "locations": process.env.LOCATIONS_URI,
+  "localhost": process.env.LOCALHOST
 }
 
-var options = {
-  router: {
-    'localhost/devices':      settings.devices   + '/devices',
-    'localhost/consumptions': settings.devices   + '/consumptions',
-    'localhost/histories':    settings.devices   + '/histories',
-    'localhost/types':        settings.types     + '/types',
-    'localhost/properties':   settings.types     + '/properties',
-    'localhost/functions':    settings.types     + '/functions',
-    'localhost/statuses':     settings.types     + '/statuses',
-    'localhost/categories':   settings.types     + '/categories',
-    'localhost/locations':    settings.locations + '/locations',
-  }
-}
+var options = { router: { } }
+
+options.router[settings.localhost + '/devices']      = settings.devices   + '/devices';
+options.router[settings.localhost + '/consumptions'] = settings.devices   + '/consumptions';
+options.router[settings.localhost + '/histories']    = settings.devices   + '/histories';
+options.router[settings.localhost + '/types']        = settings.types     + '/types';
+options.router[settings.localhost + '/properties']   = settings.types     + '/properties';
+options.router[settings.localhost + '/functions']    = settings.types     + '/functions';
+options.router[settings.localhost + '/statuses']     = settings.types     + '/statuses';
+options.router[settings.localhost + '/categories']   = settings.types     + '/categories';
+options.router[settings.localhost + '/locations']    = settings.locations + '/locations';
 
 var port   = process.env.PORT || 8000;
 var server = httpProxy.createServer(options).listen(port);
