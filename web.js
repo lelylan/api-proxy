1849621ba2de6c74b1b8b5b4cf66e55bfb91c3df
Andrea Reginato <andrea.reginato@gmail.com>
2012-08-23 16:06:45 +0200

    Added new way to set configuration vars

--- web.js
+++ web.js
@@ -1,21 +1,26 @@
 var nock = require('nock');
 var http = require('http');
 var httpProxy = require('http-proxy');
-var settings = require('konphyg')(__dirname + '/settings')('settings');
+
+settings = {
+  "devices":   process.env.DEVICES_URI,
+  "types":     process.env.TYPES_URI,
+  "locations": process.env.LOCATIONS_URI
+}
 
 var options = {
   router: {
-    'localhost/devices':      settings.devices,
-    'localhost/consumptions': settings.devices,
-    'localhost/histories':    settings.devices,
-    'localhost/types':        settings.types,
-    'localhost/properties':   settings.types,
-    'localhost/functions':    settings.types,
-    'localhost/statuses':     settings.types,
-    'localhost/categories':   settings.types,
-    'localhost/locations':    settings.locations,
+    'localhost/devices':      settings.devices   + '/devices',
+    'localhost/consumptions': settings.devices   + '/consumptions',
+    'localhost/histories':    settings.devices   + '/histories',
+    'localhost/types':        settings.types     + '/types',
+    'localhost/properties':   settings.types     + '/properties',
+    'localhost/functions':    settings.types     + '/functions',
+    'localhost/statuses':     settings.types     + '/statuses',
+    'localhost/categories':   settings.types     + '/categories',
+    'localhost/locations':    settings.locations + '/locations',
   }
 }
 
-var port   = process.env.PORT || 8000
+var port   = process.env.PORT || 8000;
 var server = httpProxy.createServer(options).listen(port);
