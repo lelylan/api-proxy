cc942023672a45398dc42baae005820c8a8017a2
Andrea Reginato <andrea.reginato@gmail.com>
2012-08-24 11:23:24 +0200

    Added new routing system through a middleware

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node web.js
+web: $NODE_COMMAND web.js
--- web.js
+++ web.js
@@ -1,26 +1,11 @@
 var httpProxy = require('http-proxy');
 
-var port   = process.env.PORT || 8000;
+var port = process.env.PORT || 8000;
 
-httpProxy.createServer(function (req, res, proxy) {
-
-  if (req.url.match(/\/devices/) || req.url.match(/\/consumptions/) || req.url.match(/\/histories/)) {
-    console.log(process.env.DEVICES_URI, process.env.DEVICES_PORT);
-    req.headers.host = process.env.DEVICES_URI;
-    proxy.proxyRequest(req, res, { port: process.env.DEVICES_PORT || 80, host: process.env.DEVICES_URI });
-  }
-
-  if (req.url.match(/\/types/) || req.url.match(/\/properties/) || req.url.match(/\/functions/) || req.url.match(/\/statuses/) || req.url.match(/\/categories/)) {
-    console.log(process.env.TYPES_URI, process.env.TYPES_PORT)
-    req.headers.host = process.env.TYPES_URI;
-    proxy.proxyRequest(req, res, { port: process.env.TYPES_PORT || 80, host: process.env.TYPES_URI });
-  }
-
-  if (req.url.match(/\/locations/)) {
-    console.log(process.env.LOCATIONS_URI, process.env.LOCATIONS_PORT)
-    req.headers.host = process.env.LOCATIONS_URI;
-    proxy.proxyRequest(req, res, { port: process.env.LOCATIONS_PORT || 80, host: process.env.LOCATIONS_URI });
-  }
-
-}).listen(port);
-
+httpProxy.createServer(
+  require('./lib/uri-middleware')({
+    '/devices':   { port: process.env.DEVICES_PORT || 80,   host: process.env.DEVICES_URI },
+    '/types':     { port: process.env.TYPES_PORT || 80,     host: process.env.TYPES_URI },
+    '/locations': { port: process.env.LOCATIONS_PORT || 80, host: process.env.LOCATIONS_URI }
+  })
+).listen(port);
