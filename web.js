cc19a4b14e70da187f036de3057f22b09a3850f8
Andrea Reginato <andrea.reginato@gmail.com>
2012-08-24 09:14:26 +0200

    Added proxyRequest functionality

--- web.js
+++ web.js
@@ -3,4 +3,10 @@ var httpProxy = require('http-proxy');
 var port   = process.env.PORT || 8000;
 
 console.log(process.env.TYPES_URI);
-httpProxy.createServer(3002, process.env.TYPES_URI).listen(port);
+//httpProxy.createServer(3002, process.env.TYPES_URI).listen(port);
+
+httpProxy.createServer(function (req, res, proxy) {
+  //req.headers.host = process.env.TYPES_URI;
+  proxy.proxyRequest(req, res, { port: process.env.TYPES_PORT, host: process.env.TYPES_URI });
+}).listen(port);
+
