387c8ab9f3bd794e82fa1661ac3da67e86605520
Andrea Reginato <andrea.reginato@gmail.com>
2012-08-24 09:36:16 +0200

    Attempt to remove host header setting

--- web.js
+++ web.js
@@ -3,10 +3,9 @@ var httpProxy = require('http-proxy');
 var port   = process.env.PORT || 8000;
 
 console.log(process.env.TYPES_URI);
-//httpProxy.createServer(3002, process.env.TYPES_URI).listen(port);
 
 httpProxy.createServer(function (req, res, proxy) {
-  req.headers.host = process.env.TYPES_URI;
+  //req.headers.host = process.env.TYPES_URI;
   proxy.proxyRequest(req, res, { port: process.env.TYPES_PORT, host: process.env.TYPES_URI });
 }).listen(port);
 
