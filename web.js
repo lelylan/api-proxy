83efecd604be65c5e2275f960227d38ee6372ea4
Andrea Reginato <andrea.reginato@gmail.com>
2012-08-24 11:31:43 +0200

    Added all routes

--- web.js
+++ web.js
@@ -4,8 +4,14 @@ var port = process.env.PORT || 8000;
 
 httpProxy.createServer(
   require('./lib/uri-middleware')({
-    '/devices':   { port: process.env.DEVICES_PORT || 80,   host: process.env.DEVICES_URI },
-    '/types':     { port: process.env.TYPES_PORT || 80,     host: process.env.TYPES_URI },
-    '/locations': { port: process.env.LOCATIONS_PORT || 80, host: process.env.LOCATIONS_URI }
+    '/devices':      { port: process.env.DEVICES_PORT || 80,   host: process.env.DEVICES_URI },
+    '/consumptions': { port: process.env.DEVICES_PORT || 80,   host: process.env.DEVICES_URI },
+    '/histories':    { port: process.env.DEVICES_PORT || 80,   host: process.env.DEVICES_URI },
+    '/types':        { port: process.env.TYPES_PORT || 80,     host: process.env.TYPES_URI },
+    '/properties':   { port: process.env.TYPES_PORT || 80,     host: process.env.TYPES_URI },
+    '/functions':    { port: process.env.TYPES_PORT || 80,     host: process.env.TYPES_URI },
+    '/statuses':     { port: process.env.TYPES_PORT || 80,     host: process.env.TYPES_URI },
+    '/categories':   { port: process.env.TYPES_PORT || 80,     host: process.env.TYPES_URI },
+    '/locations':    { port: process.env.LOCATIONS_PORT || 80, host: process.env.LOCATIONS_URI }
   })
 ).listen(port);
