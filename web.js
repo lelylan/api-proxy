d28b329ad9f889b3ecc4a94821883305677220ae
Andrea Reginato <andrea.reginato@gmail.com>
2012-08-24 09:39:49 +0200

    Back with header

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var port   = process.env.PORT || 8000;
 console.log(process.env.TYPES_URI);
 
 httpProxy.createServer(function (req, res, proxy) {
-  //req.headers.host = process.env.TYPES_URI;
+  req.headers.host = process.env.TYPES_URI;
   proxy.proxyRequest(req, res, { port: process.env.TYPES_PORT, host: process.env.TYPES_URI });
 }).listen(port);
 
