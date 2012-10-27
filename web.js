2e9e60600d031f2970fa09a86259eae81618a133
Andrea Reginato <andrea.reginato@gmail.com>
2012-10-27 14:03:13 +0200

    Fixed wrong env variable name

--- web.js
+++ web.js
@@ -12,7 +12,7 @@ var routing = {
   '/statuses':      { port: process.env.TYPES_PORT || 80,        host: process.env.TYPES_URI },
   '/categories':    { port: process.env.TYPES_PORT || 80,        host: process.env.TYPES_URI },
   '/locations':     { port: process.env.LOCATIONS_PORT || 80,    host: process.env.LOCATIONS_URI },
-  '/subscriptions': { port: process.env.SUBSCRIPTION_PORT || 80, host: process.env.SUBSCRIPTIONS_URI }
+  '/subscriptions': { port: process.env.SUBSCRIPTIONS_PORT || 80, host: process.env.SUBSCRIPTIONS_URI }
 }
 
 var server = httpProxy.createServer(
