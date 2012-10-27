31a1f64616646dd341c77d98293470b3565ec4d9
Andrea Reginato <andrea.reginato@gmail.com>
2012-10-27 13:52:54 +0200

    Added subscriptions

--- web.js
+++ web.js
@@ -3,15 +3,16 @@ var httpProxy = require('http-proxy');
 var port = process.env.PORT || 8000;
 
 var routing = {
-  '/devices':      { port: process.env.DEVICES_PORT || 80,   host: process.env.DEVICES_URI },
-  '/consumptions': { port: process.env.DEVICES_PORT || 80,   host: process.env.DEVICES_URI },
-  '/histories':    { port: process.env.DEVICES_PORT || 80,   host: process.env.DEVICES_URI },
-  '/types':        { port: process.env.TYPES_PORT || 80,     host: process.env.TYPES_URI },
-  '/properties':   { port: process.env.TYPES_PORT || 80,     host: process.env.TYPES_URI },
-  '/functions':    { port: process.env.TYPES_PORT || 80,     host: process.env.TYPES_URI },
-  '/statuses':     { port: process.env.TYPES_PORT || 80,     host: process.env.TYPES_URI },
-  '/categories':   { port: process.env.TYPES_PORT || 80,     host: process.env.TYPES_URI },
-  '/locations':    { port: process.env.LOCATIONS_PORT || 80, host: process.env.LOCATIONS_URI }
+  '/devices':       { port: process.env.DEVICES_PORT || 80,      host: process.env.DEVICES_URI },
+  '/consumptions':  { port: process.env.DEVICES_PORT || 80,      host: process.env.DEVICES_URI },
+  '/histories':     { port: process.env.DEVICES_PORT || 80,      host: process.env.DEVICES_URI },
+  '/types':         { port: process.env.TYPES_PORT || 80,        host: process.env.TYPES_URI },
+  '/properties':    { port: process.env.TYPES_PORT || 80,        host: process.env.TYPES_URI },
+  '/functions':     { port: process.env.TYPES_PORT || 80,        host: process.env.TYPES_URI },
+  '/statuses':      { port: process.env.TYPES_PORT || 80,        host: process.env.TYPES_URI },
+  '/categories':    { port: process.env.TYPES_PORT || 80,        host: process.env.TYPES_URI },
+  '/locations':     { port: process.env.LOCATIONS_PORT || 80,    host: process.env.LOCATIONS_URI }
+  '/subscriptions': { port: process.env.SUBSCRIPTION_PORT || 80, host: process.env.SUBSCRIPTIONS_URI }
 }
 
 var server = httpProxy.createServer(
