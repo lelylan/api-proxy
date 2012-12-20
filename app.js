839d06f80dcea20cb9c6211d1ae1ea5c4946af90
Andrea Reginato <andrea.reginato@gmail.com>
2012-12-20 15:49:33 +0100

    Added /me service

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var httpProxy = require('http-proxy');
 
 var port = process.env.PORT || 8000;
 
+// all not found routes are forwarded to the devices service
 var routing = {
+  '/me':            { port: process.env.PEOPLE_PORT || 80,        host: process.env.PEOPLE_URI },
   '/devices':       { port: process.env.DEVICES_PORT || 80,       host: process.env.DEVICES_URI },
   '/consumptions':  { port: process.env.DEVICES_PORT || 80,       host: process.env.DEVICES_URI },
   '/histories':     { port: process.env.DEVICES_PORT || 80,       host: process.env.DEVICES_URI },
