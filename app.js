25a17f5243eeb4dfe4f50b3988f3e47bbfc3beab
Andrea Reginato <andrea.reginato@gmail.com>
2013-05-16 10:59:55 +0200

    Updated env naming to make it more uniform

--- app.js
+++ app.js
@@ -4,18 +4,18 @@ var port = process.env.PORT || 8000;
 
 // all not found routes are forwarded to the devices service
 var routing = {
-  '/me':            { port: process.env.PEOPLE_PORT || 80,        host: process.env.PEOPLE_URI },
-  '/devices':       { port: process.env.DEVICES_PORT || 80,       host: process.env.DEVICES_URI },
-  '/consumptions':  { port: process.env.DEVICES_PORT || 80,       host: process.env.DEVICES_URI },
-  '/histories':     { port: process.env.DEVICES_PORT || 80,       host: process.env.DEVICES_URI },
-  '/activations':   { port: process.env.DEVICES_PORT || 80,       host: process.env.DEVICES_URI },
-  '/types':         { port: process.env.TYPES_PORT || 80,         host: process.env.TYPES_URI },
-  '/properties':    { port: process.env.TYPES_PORT || 80,         host: process.env.TYPES_URI },
-  '/functions':     { port: process.env.TYPES_PORT || 80,         host: process.env.TYPES_URI },
-  '/statuses':      { port: process.env.TYPES_PORT || 80,         host: process.env.TYPES_URI },
-  '/categories':    { port: process.env.TYPES_PORT || 80,         host: process.env.TYPES_URI },
-  '/locations':     { port: process.env.LOCATIONS_PORT || 80,     host: process.env.LOCATIONS_URI },
-  '/subscriptions': { port: process.env.SUBSCRIPTIONS_PORT || 80, host: process.env.SUBSCRIPTIONS_URI },
+  '/me':            { port: process.env.LELYLAN_PEOPLE_PORT || 80,        host: process.env.LELYLAN_PEOPLE_URL },
+  '/devices':       { port: process.env.LELYLAN_DEVICES_PORT || 80,       host: process.env.LELYLAN_DEVICES_URL },
+  '/consumptions':  { port: process.env.LELYLAN_DEVICES_PORT || 80,       host: process.env.LELYLAN_DEVICES_URL },
+  '/histories':     { port: process.env.LELYLAN_DEVICES_PORT || 80,       host: process.env.LELYLAN_DEVICES_URL },
+  '/activations':   { port: process.env.LELYLAN_DEVICES_PORT || 80,       host: process.env.LELYLAN_DEVICES_URL },
+  '/types':         { port: process.env.LELYLAN_TYPES_PORT || 80,         host: process.env.LELYLAN_TYPES_URL },
+  '/properties':    { port: process.env.LELYLAN_TYPES_PORT || 80,         host: process.env.LELYLAN_TYPES_URL },
+  '/functions':     { port: process.env.LELYLAN_TYPES_PORT || 80,         host: process.env.LELYLAN_TYPES_URL },
+  '/statuses':      { port: process.env.LELYLAN_TYPES_PORT || 80,         host: process.env.LELYLAN_TYPES_URL },
+  '/categories':    { port: process.env.LELYLAN_TYPES_PORT || 80,         host: process.env.LELYLAN_TYPES_URL },
+  '/locations':     { port: process.env.LELYLAN_LOCATIONS_PORT || 80,     host: process.env.LELYLAN_LOCATIONS_URL },
+  '/subscriptions': { port: process.env.LELYLAN_SUBSCRIPTIONS_PORT || 80, host: process.env.LELYLAN_SUBSCRIPTIONS_URL },
 }
 
 var server = httpProxy.createServer(
