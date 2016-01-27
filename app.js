b81d31c4e1cd95987c8d703e0ca03427c5d3446e
Andrea Reginato <andrea.reginato@gmail.com>
2016-01-27 12:27:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,15 +6,12 @@ var port = process.env.PORT || 8000;
 var routing = {
   '/me':            { port: process.env.LELYLAN_PEOPLE_PORT || 80,        host: process.env.LELYLAN_PEOPLE_URL },
   '/devices':       { port: process.env.LELYLAN_DEVICES_PORT || 80,       host: process.env.LELYLAN_DEVICES_URL },
-  '/consumptions':  { port: process.env.LELYLAN_DEVICES_PORT || 80,       host: process.env.LELYLAN_DEVICES_URL },
-  '/histories':     { port: process.env.LELYLAN_DEVICES_PORT || 80,       host: process.env.LELYLAN_DEVICES_URL },
   '/activations':   { port: process.env.LELYLAN_DEVICES_PORT || 80,       host: process.env.LELYLAN_DEVICES_URL },
   '/types':         { port: process.env.LELYLAN_TYPES_PORT || 80,         host: process.env.LELYLAN_TYPES_URL },
   '/properties':    { port: process.env.LELYLAN_TYPES_PORT || 80,         host: process.env.LELYLAN_TYPES_URL },
   '/functions':     { port: process.env.LELYLAN_TYPES_PORT || 80,         host: process.env.LELYLAN_TYPES_URL },
   '/statuses':      { port: process.env.LELYLAN_TYPES_PORT || 80,         host: process.env.LELYLAN_TYPES_URL },
   '/categories':    { port: process.env.LELYLAN_TYPES_PORT || 80,         host: process.env.LELYLAN_TYPES_URL },
-  '/locations':     { port: process.env.LELYLAN_LOCATIONS_PORT || 80,     host: process.env.LELYLAN_LOCATIONS_URL },
   '/subscriptions': { port: process.env.LELYLAN_SUBSCRIPTIONS_PORT || 80, host: process.env.LELYLAN_SUBSCRIPTIONS_URL },
 }
 
